6bb250e1043eb92b64128cda6fad9a728552c46c
aria danesh <arya717375@gmail.com>
2025-09-28 11:12:41 +0330

    Updated Android compileSdk and targetSdk to 36

--- TestAndroidJetpack/app/build.gradle.kts
+++ TestAndroidJetpack/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.github.arya458.testandroidjetpack"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.arya458.testandroidjetpack"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
