899ad44c66a09002ddc91a4fcf68e135a84203f2
aria danesh <arya717375@gmail.com>
2025-09-28 13:12:30 +0330

    version 0.0.6

--- TestAndroidJetpack/app/build.gradle.kts
+++ TestAndroidJetpack/app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    id("io.github.arya458.dead-code-detector-android")
+//    id("deadcodedetector") version "0.0.6"
 }
 
 android {
@@ -56,4 +58,14 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+
+deadCodeDetector {
+    failOnDeadCode = true
+    includeResources = true
+    keepPublicApi = true
+
+    //      ./gradlew deadCodeDetectorDebug
+    //      ./gradlew deadCodeDetectorRelease
 }
