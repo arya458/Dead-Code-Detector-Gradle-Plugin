93e4c24955720a7cc1930e76b49186e587987d18
Aria Danesh <arya717375@gmail.com>
2026-08-22 15:37:07 +0330

    chore: remove legacy model DependencyAnalyzerModel

--- Plugin/src/main/kotlin/io/github/arya458/model/DependencyAnalyzerModel.kt
+++
@@ -1,9 +0,0 @@
-package io.github.arya458.model
-
-data class DependencyAnalyzerModel (
-    val declaredDeps: Set<String>,
-    val usedDeps: Set<String>,
-    val deadDeps: Set<String>
-){
-    fun hasUnusedDependencies(): Boolean = deadDeps.isNotEmpty()
-}
